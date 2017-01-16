5da03c60bd583f6f1dfd639af0af25de67b42bed
Karla Hugo <karlabrandon817@gmail.com>
2017-01-16 11:54:45 -0600

    server started

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  var indexPath = path.join(__dirname, '../../public/views/index.html');
+  res.sendFile(indexPath);
+});
+
+// router.post('/', passport.authenticate('local'), function(req, res) {
+//     res.sendStatus(200);
+// });
+
+module.exports = router;
