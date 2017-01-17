208a09c0d75b593d8e4e2f16cb34d412ed8cd079
Karla Hugo <karlabrandon817@gmail.com>
2017-01-17 17:53:03 -0600

    register controller added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,3 +21,60 @@ myApp.config(['$routeProvider', function($routeProvider) {
             redirectTo: '/home'
         }); //end routes
 }]); //end routeProvider
+
+myApp.controller('AddController', ['$scope', '$http', function($scope, $http) {
+    console.log('in AddController');
+
+    $scope.addItem = function() {
+        var itemToSend = {
+            food_type: $scope.food_type,
+            food_pic: $scope.food_pic,
+            safeToEat: $scope.safeToEat,
+            benefits: $scope.benefits,
+            toxicity_level: $scope.toxicity_level,
+            harms: $scope.harms
+        };
+        $http.post('/addFood', itemToSend)
+            .then(function(response) {
+                console.log('Item Added');
+            }).catch(function(response) {
+                console.log('PSYCHE: ', response);
+                if (response.status === 401) {
+                    $window.location.href = '#!/login';
+                }
+
+            });
+        $scope.food_type = '';
+        $scope.food_pic = '';
+        $scope.safeToEat = '';
+        $scope.benefits = '';
+    };
+
+}]); //end AddController
+
+myApp.controller('RegisterController', ['$scope', '$http',
+    function($scope, $http) {
+        console.log('inside register controller');
+
+        $scope.register = function() {
+            var petInfo = {
+                username: $scope.username,
+                password: $scope.password,
+                pet_name: $scope.pet_name
+            };
+            console.log(petInfo);
+
+            // $http({
+            //   method: 'POST',
+            //   url: '/register',
+            //   data: userInfo
+            // }).then(function successCallback(response) {
+            //   console.log('success', response);
+            //   $window.location.href = '#!/login';
+            // }, function errorCallback(error) {
+            //   console.log('error occurred!');
+            // });
+        };
+
+    }
+]);
