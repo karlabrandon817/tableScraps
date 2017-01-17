c96f9a7c3e18d7d28fe23353be3a724863e13f80
Karla Hugo <karlabrandon817@gmail.com>
2017-01-17 17:13:51 -0600

    controllers

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,5 @@
 var myApp = angular.module('myApp', ['ngRoute']);
+angular.module('myApp.controllers', []);
 
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -14,38 +15,9 @@ myApp.config(['$routeProvider', function($routeProvider) {
         })
         .when('/register', {
             templateUrl: 'views/partials/register.html',
+            controller: 'RegisterController'
         })
         .otherwise({
             redirectTo: '/home'
         }); //end routes
 }]); //end routeProvider
-
-myApp.controller('AddController', ['$scope', '$http',  function($scope, $http){
-  console.log('in AddController');
-
-  $scope.addItem = function(){
-    var itemToSend = {
-      food_type: $scope.food_type,
-      food_pic: $scope.food_pic,
-      safeToEat: $scope.safeToEat,
-      benefits: $scope.benefits,
-      toxicity_level: $scope.toxicity_level,
-      harms: $scope.harms
-    };
-    $http.post('/addFood', itemToSend)
-    .then(function(response){
-      console.log('Item Added');
-    }).catch(function(response){
-      console.log('PSYCHE: ', response);
-      if(response.status === 401) {
-        $window.location.href = '#!/login';
-      }
-
-    });
-    $scope.food_type = '';
-    $scope.food_pic = '';
-    $scope.safeToEat = '';
-    $scope.benefits = '';
-  };
-
-}]);//end AddController
