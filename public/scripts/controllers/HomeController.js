0499fe1eb8529ceb98ba6b131f6b1d9c6211731a
Karla Hugo <karlabrandon817@gmail.com>
2017-01-19 10:17:03 -0600

    search results displaying on dom

--- public/scripts/controllers/HomeController.js
+++ public/scripts/controllers/HomeController.js
@@ -1,21 +1,21 @@
 angular.module('myApp').controller('HomeController', ['$scope', '$http', '$window', function($scope, $http, $window) {
- console.log('in HomeController');
+    console.log('in HomeController');
 
-$scope.search = function(){
-  console.log('search button clicked');
-  var searchToSend = {
-    food_type: $scope.searchedFood.toLowerCase()
-  };//end searchToSend
-  console.log('searchToSend:', searchToSend);
+    $scope.search = function() {
+        console.log('search button clicked');
+        var searchToSend = {
+            food_type: $scope.searchedFood.toLowerCase()
+        }; //end searchToSend
+        console.log('searchToSend:', searchToSend);
 
 
-$http.post('/search', searchToSend)
-.then(function(response){
-  console.log(response);
-}).catch(function(response){
-console.log(response);
-});
-
-};//end scope.search
+        $http.post('/search', searchToSend)
+            .then(function(response) {
+                console.log(response);
+                  $scope.foods = response.data;
+            }).catch(function(response) {
+                console.log(response);
+            });
+    }; //end scope.search
 
 }]); //end HomeController
